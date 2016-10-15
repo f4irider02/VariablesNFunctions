90971f20391e487ece6926a67c933ebd234c6a23
f4irider02 <grey90stanggt@gmail.com>
2016-10-15 11:33:01 -0600

    Add files via upload

---
+++ VariablesNFunctions.cs
@@ -0,0 +1,21 @@
+﻿int ammo = 80;
+int health = 100;
+bool awesome;
+float powerUp;
+string name;
+
+void Start()
+{
+    powerUp = 0;
+    awesome = false;
+}
+
+public int GetAmmo()
+{
+    return ammo;
+}
+
+void UsePowerUp()
+{
+    powerUp--;
+}
